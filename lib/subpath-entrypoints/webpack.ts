31d70b2bc25da06a2da8e89fbbf24bc2f5a1f372
Jonas Amundsen <jonasba@gmail.com>
2024-09-19 17:38:50 +0200

    Use provided `LoaderDefinition` from webpack
    This now exists (and admittedly has for some time).

--- lib/subpath-entrypoints/webpack.ts
+++ lib/subpath-entrypoints/webpack.ts
@@ -1,23 +1,11 @@
+import type { LoaderDefinition } from "webpack";
+
 import { compile } from "../template";
 
-/**
- * Can be removed once Webpack v5 is supported by Cypress' preprocessor, because Webpack v5 ships
- * with these types, ref. https://github.com/webpack/webpack/pull/13164.
- */
-interface ILoaderContext {
-  async: () => (err: Error | null, result?: string) => void;
-  resourcePath: string;
-  query: any;
-}
-
-interface ILoaderDefinition {
-  (this: ILoaderContext, data: string): void;
-}
-
-const loader: ILoaderDefinition = function (data) {
+const loader: LoaderDefinition = function (data) {
   const callback = this.async();
 
-  compile(this.query, data, this.resourcePath).then(
+  compile(this.query as any, data, this.resourcePath).then(
     (result) => callback(null, result),
     (error) => callback(error),
   );
